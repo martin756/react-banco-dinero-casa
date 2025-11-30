492874ae8483dd15af4f7824d53d5093403474b4
magostino <martinagostino75@yahoo.com.ar>
2025-11-29 23:04:26 -0300

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: '/react-banco-dinero-casa/',  // 👈 Ruta del repositorio
+    cleanDistPath: true,
+  },
 });
